1fa6c3c323664b57b6cd63eace7498988ff974c8
AK2b_9yMgt*9uKj-m <mahmud102496@gmail.com>
2026-09-10 21:26:53 +0600

    _

---
+++ recap.js
@@ -0,0 +1,17 @@
+/**
+ * 1. ver let const
+ * 2. default parameter 
+ * 3. template string
+ */
+
+const a= 56;
+const number =[56, 78, 90, 45, 23, 12];
+
+
+const person = {
+    name : 'John'
+}
+ 
+const massage = `hi , ${person.name} has a " ${a} access to ${number[2]} " `;
+
+console.log(massage);
