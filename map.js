954d04c621916637abf36670823f654c86bc5227
AK2b_9yMgt*9uKj-m <mahmud102496@gmail.com>
2026-09-15 03:52:59 +0600

    map

---
+++ map.js
@@ -0,0 +1,8 @@
+const numbers = [1, 2, 3, 4, 5];
+
+const double =[];
+for (const num of numbers) {
+    const doublel = num * 2;
+    double.push(doublel);
+}
+console.log(double); // Output: [2, 4, 6, 8, 10]
