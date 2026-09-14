c573a8b7ab347adb5b7f6feeac553478e6ef3af4
AK2b_9yMgt*9uKj-m <mahmud102496@gmail.com>
2026-09-15 03:55:17 +0600

    map=

--- map.js
+++ map.js
@@ -1,8 +1,15 @@
 const numbers = [1, 2, 3, 4, 5];
 
-const double =[];
-for (const num of numbers) {
-    const doublel = num * 2;
-    double.push(doublel);
+// const double =[];
+// for (const num of numbers) {
+//     const doublel = num * 2;
+//     double.push(doublel);
+// }
+// console.log(double); // Output: [2, 4, 6, 8, 10]
+
+function doubleit(num) {
+    return num * 2;
 }
-console.log(double); // Output: [2, 4, 6, 8, 10]
+
+const result = numbers.map(doubleit);
+console.log(result); // Output: [2, 4, 6, 8, 10]
